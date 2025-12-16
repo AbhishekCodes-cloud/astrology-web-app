b0053d90c78d612f984fd59d91ce3088ba4f12c2
Abhishek <abhishekpal46@gmail.com>
2025-12-17 05:28:06 +0530

    Pushing app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React Astrology</h1>
+      <h1>Vite + React Astrology web App</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
